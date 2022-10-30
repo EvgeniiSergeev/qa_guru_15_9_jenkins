0a59f90d1e7c88be74b3567a8575d8109bb0df3f
Evgenii <sergeevjane@mail.ru>
2022-10-30 22:58:48 +0300

    added update HW

---
+++ src/test/java/com/demoqa/BaseTest.java
@@ -0,0 +1,43 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.AllureAttachments;
+
+import io.qameta.allure.selenide.AllureSelenide;
+
+public class BaseTest {
+
+    /**
+     * метод сет-ап, который выполнится перед всеми тестами
+     */
+    @BeforeAll
+    static void setUp() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        // Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+
+        AllureAttachments.screenshotAs("Последний скриншот");
+        AllureAttachments.pageSource();
+        AllureAttachments.browserConsoleLogs();
+        AllureAttachments.addVideo();
+    }
+
+}
--- src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
@@ -1,30 +1,26 @@
 package com.demoqa.tests;
 
-import com.codeborne.selenide.Condition;
+import static io.qameta.allure.Allure.step;
 import com.codeborne.selenide.Configuration;
+import com.demoqa.BaseTest;
 import com.demoqa.pages.RegistrationFormPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-
-public class RegistrationFormWithPageObjectsTests {
+public class RegistrationFormWithPageObjectsTests extends BaseTest {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-
     @BeforeAll
     static void configure() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
     }
-
-
     @Test
+    @DisplayName("Заполнение всех полей регистрационной формы")
     void fillFormTest() {
-        registrationFormPage.openPage()
+        step("Заполнение формы", () -> {
+            registrationFormPage.openPage()
                 .setFirstName("Alex")
                 .setLastName("Egorov")
                 .setEmail("alex@egorov.com")
@@ -38,18 +34,10 @@ public class RegistrationFormWithPageObjectsTests {
                 .setState("NCR")
                 .setCity("Delhi")
                 .clickSubmitButton();
+        });
 
-//        $("#subjectsInput").setValue("Maths").pressEnter();
-//        $("#hobbiesWrapper").$(byText("Sports")).click(); // best
-//        $("#uploadPicture").uploadFromClasspath("img/1.png");
-//        $("#currentAddress").setValue("Some address 1");
-//        $("#state").click();
-//        $("#stateCity-wrapper").$(byText("NCR")).click();
-//        $("#city").click();
-//        $("#stateCity-wrapper").$(byText("Delhi")).click();
-//        $("#submit").click();
-
-        registrationFormPage.checkResultsTableVisible()
+        step("Проверяем результат отправки формы", () -> {
+            registrationFormPage.checkResultsTableVisible()
                 .checkResult("Student Name", "Alex Egorov")
                 .checkResult("Student Email", "alex@egorov.com")
                 .checkResult("Date of Birth", "30 July,2008")
@@ -60,32 +48,28 @@ public class RegistrationFormWithPageObjectsTests {
                 .checkResult("Picture", "Picture 1.png")
                 .checkResult("Address", "Some address 1")
                 .checkResult("State and City", "NCR Delhi");
+        });
     }
-
     @Test
     void fillFormWithMinimumDataTest() {
-        registrationFormPage.openPage()
+        step("Заполнение обязательных полей формы", () -> {
+            registrationFormPage.openPage()
                 .setFirstName("Alex")
                 .setLastName("Egorov")
                 .setGender("Other")
                 .setNumber("1234567890")
                 .setBirthDate("30", "July", "2008")
                 .clickSubmitButton();
+        });
 
-
-        registrationFormPage.checkResultsTableVisible()
+        step("Проверяем результат отправки формы", () -> {
+            registrationFormPage.checkResultsTableVisible()
                 .checkResult("Student Name", "Alex Egorov")
                 .checkResult("Date of Birth", "30 July,2008")
                 .checkResult("Gender", "Other")
                 .checkResult("Mobile", "1234567890");
+        });
 
-//        $("#userNumber").setValue("1234567890");
-//        $("#submit").click();
-//
-//        $(".modal-dialog").should(appear);
-//        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-//
-//        $(".table-responsive table").shouldHave(text("Alex"), text("Egorov"), text("1234567890"));
     }
 
 }
